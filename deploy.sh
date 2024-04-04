a0cae680b98fea8c25dcd4146f9350d68dd8be6a
Noah Bertholon <noahbertholon@gmail.com>
2024-04-04 16:28:07 +0200

    Les variables git pas utilisable dans un sciprt ?

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ -f dev_envr ]; then
     certificate='../gitlabci.pem'
     path='.'
 else
-    certificate='${CI_GITLAB_KEY}'
-    path='${CI_PROJECT_DIR}'
+    certificate='$CI_GITLAB_KEY'
+    path='$CI_PROJECT_DIR'
 fi
 
 #scp -i "$certificate" api/server/index.js api/server/queries.js api/server/routers.js gitlabci@192.168.75.17:/opt/backend
