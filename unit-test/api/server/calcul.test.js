15096e251871c2b6db9e83447a5790f207a62110
Ouliviaa <oliviachen0815@gmail.com>
2024-04-23 14:07:02 +0200

    correction test => changement de nom de fonction

--- unit-test/api/server/calcul.test.js
+++ unit-test/api/server/calcul.test.js
@@ -1,9 +1,9 @@
 // footprint.test.js
-const { footprintBoisson, footprintBoissons } = require('../../../api/server/calcul');
+const { footprint, footprintBoissons } = require('../../../api/server/calcul');
 
 // Test for footprintBoisson function
 test('calculates footprint for a single beverage correctly', () => {
-  expect(footprintBoisson(10, 2)).toBe(20); // Assuming footprint of 10 and quantity of 2
+  expect(footprint(10, 2)).toBe(20); // Assuming footprint of 10 and quantity of 2
 });
 
 // Test for footprintBoissons function
