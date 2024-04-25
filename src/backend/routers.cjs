7881e4e8fdb1525e1cbd1c3a7190d86120ee1cb3
Ouliviaa <oliviachen0815@gmail.com>
2024-04-25 17:12:47 +0200

    adaptation des router.js

--- src/backend/routers.cjs
+++ src/backend/routers.cjs
@@ -6,43 +6,43 @@ const cors = require("cors");
 const origin = '*'
 
 //TODO : a enlever
-router.get('/test', db.getTest);
+router.get('/api/test', db.getTest);
 
 //get ecv d'un name(objet) de slug(theme)
-router.get('/carbone/:slug/:name', db.getCarbonne);
+router.get('/api/carbone/:slug/:name', db.getCarbonne);
 
 //get footprint de slug(theme)
-router.get('/footprint/:slug', db.getFootPrint);
+router.get('/api/footprint/:slug', db.getFootPrint);
 
-router.get('/ecv/boissons', db.getBoissonsEcv);
+router.get('/api/ecv/boissons', db.getBoissonsEcv);
 
-router.get('/ecv/electromenager', db.getElectromenager);
+router.get('/api/ecv/electromenager', db.getElectromenager);
 
-router.get('/ecv/repas', db.getRepas);
+router.get('/api/ecv/repas', db.getRepas);
 
-router.get('/ecv/transport', db.getTransport);
+router.get('/api/ecv/transport', db.getTransport);
 
-router.get('/ecv/chauffage', db.getChauffage);
+router.get('/api/ecv/chauffage', db.getChauffage);
 
-router.get('/ecv/fruitsetlegumes', db.getFruitsetLegumesEcv);
+router.get('/api/ecv/fruitsetlegumes', db.getFruitsetLegumesEcv);
 
-router.get('/ecv/numerique', db.getNumeriqueEcv);
+router.get('/api/ecv/numerique', db.getNumeriqueEcv);
 
-router.get('/ecv/mobilier', db.getMobilierEcv);
+router.get('/api/ecv/mobilier', db.getMobilierEcv);
 
-router.get('/ecv/usagenumerique', db.getUsageNumeriqueEcv);
+router.get('/api/ecv/usagenumerique', db.getUsageNumeriqueEcv);
 
-router.get('/ecv/vetements', db.getVetements);
+router.get('/api/ecv/vetements', db.getVetements);
 
-router.get('/ecv/eaux', db.getEaux);
+router.get('/api/ecv/eaux', db.getEaux);
 
 //effacer les données
-router.get('/delete', cors({ origin: origin }), db.deleteData);
+router.get('/api/delete', cors({ origin: origin }), db.deleteData);
 
 //insérer les données de l'api dans la base de données
-router.get('/insert', cors({ origin: origin}), db.insertAll)
+router.get('/api/insert', cors({ origin: origin}), db.insertAll)
 
 //créer les tables de données
-router.get('/create', cors({ origin: origin }), db.createTables)
+router.get('/api/create', cors({ origin: origin }), db.createTables)
 
 module.exports = router;
