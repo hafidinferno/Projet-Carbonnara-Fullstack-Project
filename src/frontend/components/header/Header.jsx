8478d48f094fd268a784917eb0b7b588c5a73f71
Noah Bertholon <noahbertholon@gmail.com>
2024-04-30 10:43:20 +0200

    Navigation en fleur

--- src/frontend/components/header/Header.jsx
+++ src/frontend/components/header/Header.jsx
@@ -21,26 +21,19 @@ function Header() {
   return (
     <header className="App App-header shadowed">
       <div className="circle-container">
-        <button
-          onClick={startTest}
-          className="sub-circle shadowed"
-          id="circle1"
-        >
-          TEST
-        </button>
-        <button
-          onClick={GoResults}
-          className="sub-circle shadowed"
-          id="circle2"
-        >
-          RESULTS
-        </button>
-        <button onClick={GoAbout} className="sub-circle shadowed" id="circle3">
-          About US
-        </button>
         <button onClick={GoAccueil} className="circle shadowed">
           Accueil
         </button>
+        <button onClick={startTest} className="sub-circle shadowed" id="circle1">
+          <p className="sub-circle-content">Test</p>
+        </button>
+        <button onClick={GoResults} className="sub-circle shadowed" id="circle2">
+          <p className="sub-circle-content">Result</p>
+        </button>
+        <button onClick={GoAbout} className="sub-circle shadowed" id="circle3">
+          <p className="sub-circle-content">About</p>
+        </button>
+        
       </div>
       <h1>CARBONARA</h1>
     </header>
